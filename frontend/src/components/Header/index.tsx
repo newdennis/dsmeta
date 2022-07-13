5438560249a1647552ad4986ea88e6b5a40d1854
newdennis <newdennis@gmail.com>
2022-07-12 21:39:33 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
+
+function Header() {
+    return (
+
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="logo" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvoldio por: <a target="_blank" href="https://www.instagram.com/wd.lima">@Wellingtonlima</a>
+                </p>
+            </div>
+        </header>
+
+
+    )
+}
+
+export default Header
